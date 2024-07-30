d908fc158c74dd1773af12ebb3d7254fd2ed03f6
SamLoureiro <samuelloureiro1@gmail.com>
2024-07-30 14:58:45 +0100

    bash script to remove files from git and PC

---
+++ update_git_files.sh
@@ -0,0 +1,33 @@
+git rm CNN_Merged_1s.py
+
+git rm Pre_Proc_Audio.py
+
+git rm RandomForest_1s.py
+
+git rm TFModel_10s.py
+
+git rm TFModel_ACEL_FFT.py
+
+git rm TFModel_ACEL.py
+
+git rm TFModel_Merged_10s.py
+
+git rm TFModel_MIC.py
+
+git commit -m "remove CNN_Merged_1s.py"
+
+git commit -m "remove Pre_Proc_Audio.py"
+
+git commit -m "remove RandomForest_1s.py"
+
+git commit -m "remove TFModel_10s.py"
+
+git commit -m "remove TFModel_ACEL_FFT.py"
+
+git commit -m "remove TFModel_ACEL.py"
+
+git commit -m "remove TFModel_Merged_10s.py"
+
+git commit -m "remove TFModel_MIC.py"
+
+git push
